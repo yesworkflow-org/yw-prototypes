3a28d58da09f36b068441d636ea0a9cb72edaa5f
Timothy McPhillips <tmcphillips@absoluteflow.org>
2014-12-18 18:15:41 -0800

    Added base class for YesWorkflow tests that capture standard input and output streams.

---
+++ src/main/java/org/yesworkflow/util/YesWorkflowTestCase.java
@@ -0,0 +1,34 @@
+package org.yesworkflow.util;
+
+/* This file is an adaptation of KuratorAkkaTestCase.java in the org.kurator.akka
+ * package as of 18Dec2014.
+ */
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.ByteArrayOutputStream;
+
+import junit.framework.TestCase;
+
+public class YesWorkflowTestCase extends TestCase {
+    
+    public static final String EOL = System.getProperty("line.separator");
+
+    protected OutputStream stdoutBuffer;
+    protected OutputStream stderrBuffer;
+    
+    protected PrintStream stdoutStream;
+    protected PrintStream stderrStream;
+
+    @Override
+    public void setUp() {
+
+//        KuratorAkka.enableLog4J();
+        
+        stdoutBuffer = new ByteArrayOutputStream();
+        stdoutStream = new PrintStream(stdoutBuffer);
+    
+        stderrBuffer = new ByteArrayOutputStream();
+        stderrStream = new PrintStream(stderrBuffer);
+    }
+}
