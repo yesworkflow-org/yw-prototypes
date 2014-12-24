0f471553b55c7d35174e20df6a699d27295115b6
Timothy McPhillips <tmcphillips@absoluteflow.org>
2014-12-24 11:11:01 -0800

    Added methods to Extractor interface.  Can now set the comment character and access extracted comment lines trimmed of language-specific comment characters and white space.

--- src/main/java/org/yesworkflow/Extractor.java
+++ src/main/java/org/yesworkflow/Extractor.java
@@ -1,7 +1,11 @@
 package org.yesworkflow;
 
+import java.util.List;
+
 public interface Extractor {
-    public Extractor sourcePath(String path);
-    public Extractor databasePath(String path);
-    public void extract() throws Exception;
+    Extractor commentCharacter(char c);
+    Extractor sourcePath(String path);
+    Extractor databasePath(String path);
+    void extract() throws Exception;
+    List<String> getLines();
 }
