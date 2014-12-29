594821caa8df46ffa6fe8c3dc583ba8caef1fcdd
Timothy McPhillips <tmcphillips@absoluteflow.org>
2014-12-29 14:27:30 -0800

    A Program object now includes references to the EndComment for the program, and to the in and out ports for the program.

--- src/main/java/org/yesworkflow/model/Program.java
+++ src/main/java/org/yesworkflow/model/Program.java
@@ -1,12 +1,21 @@
 package org.yesworkflow.model;
 
+import java.util.List;
+
 import org.yesworkflow.comments.BeginComment;
+import org.yesworkflow.comments.EndComment;
 
 public class Program {
 
-	public final BeginComment beginComment;
-	
-	public Program (BeginComment comment) {
-		this.beginComment = comment;
+    public final BeginComment beginComment;
+    public final EndComment endComment;
+    public final Port[] inPorts;
+    public final Port[] outPorts;
+    	
+	public Program (BeginComment beginComment, EndComment endComment, List<Port> inPorts, List<Port> outPorts) {
+		this.beginComment = beginComment;
+        this.endComment = endComment;
+	    this.inPorts = inPorts.toArray(new Port[inPorts.size()]);
+	    this.outPorts = outPorts.toArray(new Port[outPorts.size()]);
 	}
 }
