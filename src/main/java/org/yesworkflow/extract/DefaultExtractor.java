8a105e312c4f819bd1b7e03bb8c9e17b14d33b7f
yang <sycao5@gmail.com>
2015-01-06 10:31:20 -0600

    Update DefaultExtractor.java

--- src/main/java/org/yesworkflow/extract/DefaultExtractor.java
+++ src/main/java/org/yesworkflow/extract/DefaultExtractor.java
@@ -1,6 +1,7 @@
 package org.yesworkflow.extract;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
