47a13283b584aba93fab498e86a419b985d8a965
swanskysong <swanskysong@gmail.com>
2015-01-06 08:15:02 -0800

    Add simple exmaple script

---
+++ src/test/resources/simpleExample.py
@@ -0,0 +1,27 @@
+__author__ = 'tianhong'
+
+
+## @begin main
+
+x=1
+
+## @begin 1a
+#  @in x as X
+#  @out y @as Y
+
+y=x+1
+
+## @end 1a
+
+## @begin 2b
+#  @in y as Y
+#  @out z @as Z
+
+z=y+1
+
+## @end 2b
+
+
+
+
+## @end main
