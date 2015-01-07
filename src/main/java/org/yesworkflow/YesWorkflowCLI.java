537fcad68f28e5c91f5654e071f2fcedb582dbcd
yang <sycao5@gmail.com>
2015-01-06 23:45:42 -0600

    Update YesWorkflowCLI.java
    move fileCommentCharacter() to YesWorkflowCLI.java and add extractCommentCharacter(String path) method

--- src/main/java/org/yesworkflow/YesWorkflowCLI.java
+++ src/main/java/org/yesworkflow/YesWorkflowCLI.java
@@ -6,6 +6,7 @@ package org.yesworkflow;
 
 import static java.util.Arrays.asList;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -31,7 +32,7 @@ public class YesWorkflowCLI {
     public static final String EOL = System.getProperty("line.separator");
 
     public static void main(String[] args) throws Exception {
-        
+
         Integer returnValue = null;
 
         try {
@@ -40,10 +41,10 @@ public class YesWorkflowCLI {
             e.printStackTrace();
             returnValue = YW_CLI_UNCAUGHT_EXCEPTION;
         }
-        
+
         System.exit(returnValue);
     }
-    
+
     private PrintStream errStream;
     private OptionParser parser = null;
     private OptionSet options = null;
@@ -52,6 +53,7 @@ public class YesWorkflowCLI {
     private String databaseFilePath = null;
     private Extractor extractor = null;
     private PrintStream outStream;
+    private char comment_char = 0;
 
     public YesWorkflowCLI() throws Exception {
         this(System.out, System.err);
@@ -69,9 +71,9 @@ public class YesWorkflowCLI {
     }
 
     public int runForArgs(String[] args) throws Exception {
-        
+
         initialize();
-        
+
         try {
 
             // parse the command line arguments and options
@@ -80,23 +82,25 @@ public class YesWorkflowCLI {
             } catch (OptionException exception) {
                 throw new UsageException(exception.getMessage());
             }
-            
+
             // print help and exit if requested
             if (options.has("h")) {
                 errStream.println();
                 parser.printHelpOn(errStream);
-                return YW_CLI_SUCCESS;            
+                return YW_CLI_SUCCESS;
             }
-            
+
             // extract YesWorkflow command from arguments
-            extractCommandFromOptions();                            
+            extractCommandFromOptions();
             if (command == null) {
                 throw new UsageException("No command provided to YesWorkflow");
             }
-        
+
             // extract remaining arguments
             extractSourcePathFromOptions();
             extractDatabasePathFromOptions();
+            // extract comment character
+            extractCommentCharacter(sourceFilePath);
 
             // run extractor and exit if extract command given
             if (command.equals("extract")) {
@@ -109,8 +113,8 @@ public class YesWorkflowCLI {
                 graph();
                 return YW_CLI_SUCCESS;
             }
-            
-            
+
+
         } catch (UsageException ue) {
             errStream.print("Usage error: ");
             errStream.println(ue.getMessage());
@@ -118,21 +122,21 @@ public class YesWorkflowCLI {
             parser.printHelpOn(errStream);
             return YW_CLI_USAGE_ERROR;
         }
-        
+
         return YW_CLI_SUCCESS;
     }
-    
+
     private void initialize() {
         options = null;
         command = null;
         sourceFilePath = null;
         databaseFilePath = null;
     }
-    
+
     private void extractCommandFromOptions() {
 
         if (options.nonOptionArguments().size() == 1) {
-            
+
             // if there is only one non-option argument assume this is the command to YesWorkflow
             command = (String) options.nonOptionArguments().get(0);
 
@@ -141,6 +145,46 @@ public class YesWorkflowCLI {
             // otherwise use the argument to the -c option
             command = (String) options.valueOf("c");
         }
+    }
+
+    /**
+     * This method gets the comment character based on file extension
+     * @param path
+     * @return file extension
+     */
+	public char findCommentCharacter(String sourcePath){
+
+        String fileName = new File(sourcePath).getName();
+        int i = fileName.lastIndexOf(".");
+        String ext = null; // get file extension
+        char c = 0;
+
+        if (i == -1) {
+        	c = '#';
+        	return c;
+        }
+
+        ext = fileName.substring(i+1);
+		if(ext.equalsIgnoreCase("py")){
+    		c = '#';
+    	} else if(ext.equalsIgnoreCase("R")){
+    		c = '#';
+    	} else if(ext.equalsIgnoreCase("java")) {
+    		c = '/';
+    	} else if(ext.equalsIgnoreCase("m")) {
+    		c = '%';
+    	} else {
+    		// nothing happen here
+    	}
+		return c;
+	}
+
+    private void extractCommentCharacter(String path) {
+    	if(options.hasArgument("x")) {// check the comment character from -x option
+       		comment_char = ((String)options.valueOf("x")).charAt(0);
+    	} else {
+    		comment_char = findCommentCharacter(path); // get comment character based on file extension
+    	}
     }
 
     private void extractDatabasePathFromOptions() {
@@ -156,13 +200,18 @@ public class YesWorkflowCLI {
     private OptionParser createOptionsParser() throws Exception {
 
         OptionParser parser = null;
-        
+
         parser = new OptionParser() {{
-            
+
             acceptsAll(asList("c", "command"), "command to YesWorkflow")
                 .withRequiredArg()
                 .ofType(String.class)
                 .describedAs("command");
+
+            acceptsAll(asList("x", "commchar"), "comment character")
+        		.withOptionalArg()
+        		.ofType(String.class)
+        		.describedAs("comment");
 
             acceptsAll(asList("s", "source"), "path to source file to analyze")
                 .withOptionalArg()
@@ -180,7 +229,7 @@ public class YesWorkflowCLI {
                 .defaultsTo("-")
                 .ofType(String.class)
                 .describedAs("dot file");
-                            
+
             acceptsAll(asList("l", "lines"), "path to file for saving extracted comment lines")
                 .withOptionalArg()
                 .defaultsTo("-")
@@ -190,40 +239,40 @@ public class YesWorkflowCLI {
             acceptsAll(asList("h", "help"), "display help");
 
         }};
-            
+
         return parser;
     }
-    
+
     public void extract() throws Exception {
         char c = 0;
-        
+
         if (extractor == null) {
            extractor = new DefaultExtractor();
         }
-        
+
         if (sourceFilePath.equals("-")) {
         	extractor.sourceReader(new InputStreamReader(System.in));
         } else {
         	extractor.sourcePath(sourceFilePath);
-        	c = extractor.getCommentCharacter(); // get comment character based on file extension 
+        	databaseFilePath = (String) options.valueOf("d");
         }
-        
+
         extractor.databasePath(databaseFilePath)
-        		 .commentCharacter(c) 
+        		 .commentCharacter(comment_char)
         		 .extract();
-        
+
         if (options.has("l")) {
-            
+
             StringBuffer linesBuffer = new StringBuffer();
             for (String line : extractor.getLines()) {
                 linesBuffer.append(line);
                 linesBuffer.append(EOL);
             }
-            
+
             writeTextToOptionNamedFile("l", linesBuffer.toString());
         }
     }
-    
+
     public void writeTextToOptionNamedFile(String option, String text) throws IOException {
         String path = (String) options.valueOf(option);
         PrintStream linesOutputStream = (path.equals("-")) ? outStream : new PrintStream(path);
@@ -234,15 +283,19 @@ public class YesWorkflowCLI {
     }
 
     public void graph() throws Exception {
-        
+
         Program program = extractor.getProgram();
-        
+
         String graph = new DotGrapher()
             .workflow((Workflow)program)
             .type(GraphType.DATA_FLOW_GRAPH)
             .graph()
             .toString();
-        
+
         writeTextToOptionNamedFile("g", graph);
+    }
+
+    public Extractor getExtractor() {
+    	return extractor;
     }
 }
