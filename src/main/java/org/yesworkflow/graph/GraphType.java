d81748dbb2daa19d3cb70460047c6b1309961576
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-01-13 21:57:57 -0800

    Renamed GraphView enumeration and its members.

--- src/main/java/org/yesworkflow/graph/GraphType.java
+++
@@ -1,7 +0,0 @@
-package org.yesworkflow.graph;
-
-public enum GraphType { 
-    DATA_FLOW_GRAPH,
-    DATA_DEPENDENCY_GRAPH, 
-    ABSTRACT_PROVENANCE_GRAPH
-}
---
+++ src/main/java/org/yesworkflow/graph/GraphView.java
@@ -0,0 +1,7 @@
+package org.yesworkflow.graph;
+
+public enum GraphView { 
+    PROCESS_CENTRIC_VIEW,
+    DATA_CENTRIC_VIEW, 
+    COMBINED_VIEW
+}
