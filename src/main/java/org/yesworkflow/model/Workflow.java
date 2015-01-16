4a0acfe887534d1a2b5430220111ae5dcadad66b
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-01-15 18:31:10 -0800

    Workflow.Builder now warns of unbound ports.
    Warnings are sent to stderr if input and output ports on workflow do not
    all bind to input and output ports on programs in workflow.

--- src/main/java/org/yesworkflow/model/Workflow.java
+++ src/main/java/org/yesworkflow/model/Workflow.java
@@ -6,6 +6,7 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.yesworkflow.comments.BeginComment;
 import org.yesworkflow.comments.EndComment;
@@ -35,15 +36,13 @@ public class Workflow extends Program {
 		
         private BeginComment beginComment;
         private EndComment endComment;
-        private List<Port> inPorts = new LinkedList<Port>();
-        private List<Port> outPorts = new LinkedList<Port>();
+        private List<Port> workflowInPorts = new LinkedList<Port>();
+        private List<Port> workflowOutPorts = new LinkedList<Port>();
 
 		private List<Program> nestedPrograms = new LinkedList<Program>();
         private List<Channel> nestedChannels = new LinkedList<Channel>();
-		private Map<String,List<Port>> nestedInPorts = new LinkedHashMap<String,List<Port>>();
-		private Map<String,Port> nestedOutPorts = new  LinkedHashMap<String,Port>();
-
-        private Map<Port,String> programNameForPort = new HashMap<Port,String>();
+		private Map<String,List<Port>> nestedProgramInPorts = new LinkedHashMap<String,List<Port>>();
+		private Map<String,Port> nestedProgramOutPorts = new  LinkedHashMap<String,Port>();
         private Map<String,Program> programForName = new HashMap<String,Program>();
         
         private PrintStream stdoutStream = null;
@@ -76,12 +75,12 @@ public class Workflow extends Program {
         public Port inPort(InComment inComment) throws Exception {
             
             // model the outward facing in port
-            Port inPort = new Port(inComment);
-            inPorts.add(inPort);
+            Port inPort = new Port(inComment, beginComment);
+            workflowInPorts.add(inPort);
             
             // model a corresponding, inward-facing out port
-            Port outPort = new Port(inComment);
-            nestedOutPort(outPort, this.beginComment.programName);
+            Port outPort = new Port(inComment, beginComment);
+            nestedOutPort(outPort);
 
             // return the outward facing port
             return inPort;
@@ -90,45 +89,43 @@ public class Workflow extends Program {
         public Port outPort(OutComment outComment) {
 
             // model the outward facing out port
-            Port outPort = new Port(outComment);
-            outPorts.add(outPort);
+            Port outPort = new Port(outComment, beginComment);
+            workflowOutPorts.add(outPort);
             
             // model a corresponding, inward-facing in port
-            Port inPort = new Port(outComment);
-            nestedInPort(inPort, this.beginComment.programName);
+            Port inPort = new Port(outComment, beginComment);
+            nestedInPort(inPort);
             
             // return the outward facing port
             return outPort;
         }
         
-		public Builder nestedInPort(Port inPort, String programName) {
-		    String binding = inPort.comment.binding();
+		public Builder nestedInPort(Port inPort) {
+		    String binding = inPort.portComment.binding();
 			addNestedInport(binding, inPort);
-			this.programNameForPort.put(inPort, programName);
 			return this;
 		}
 
 		private void addNestedInport(String binding, Port inPort) {
-			List<Port> ports = this.nestedInPorts.get(binding);
+			List<Port> ports = this.nestedProgramInPorts.get(binding);
 			if (ports == null) {
 				ports = new LinkedList<Port>();
-				this.nestedInPorts.put(binding, ports);
+				this.nestedProgramInPorts.put(binding, ports);
 			}
 			ports.add(inPort);
 		}
 		
-		public Builder nestedOutPort(Port outPort, String programName) throws Exception {
+		public Builder nestedOutPort(Port outPort) throws Exception {
 			
-			String binding = outPort.comment.binding();
+			String binding = outPort.portComment.binding();
 			
 			// ensure no other writers to this @out binding
-			if (nestedOutPorts.containsKey(binding)) {
+			if (nestedProgramOutPorts.containsKey(binding)) {
 				throw new Exception("Multiple @out comments bound to " + binding);
 			}
 			
 			// store the @out comment
-			this.nestedOutPorts.put(binding, outPort);
-			this.programNameForPort.put(outPort, programName);
+			this.nestedProgramOutPorts.put(binding, outPort);
 
 			return this;
 		}
@@ -137,80 +134,96 @@ public class Workflow extends Program {
 			
 			// if no subprograms then we're building a simple program
 			if (nestedPrograms.size() == 0) {				
-				return new Program(beginComment, endComment, inPorts, outPorts);
+				return new Program(beginComment, endComment, workflowInPorts, workflowOutPorts);
 			}
 			
-			// remove from model any workflow in ports with no corresponding nested in ports
-			for (Port port : inPorts) {
-			    if (nestedInPorts.get(port.comment.binding()) == null) {
-                    
-			        stderrStream.println(
-                            "WARNING: No nested @in corresponding to workflow @in '" +
-                            port.comment.binding()                                   +
-                            "' on '"                                                 +
-                            beginComment.programName                                 +
-                            "'"
-                    );
-
-                    inPorts.remove(port);
-			    }
-			}
-
-            // remove from model any workflow in ports with no corresponding nested in ports
-            for (Port port : outPorts) {
-                if (nestedOutPorts.get(port.comment.binding()) == null) {
-                    
-                    stderrStream.println(
-                            "WARNING: No nested @out corresponding to workflow @out '" +
-                            port.comment.binding()                                   +
-                            "' on '"                                                 +
-                            beginComment.programName                                 +
-                            "'"
-                    );
-
-                    outPorts.remove(port);
-                }
-            }			
-			
-			// build the channels between in and out ports
-			for (Map.Entry<String, List<Port>> entry : nestedInPorts.entrySet()) {
-
-				String binding = entry.getKey();
-				List<Port> boundInPorts = entry.getValue();
-
-				// get information about the @out port that writes to this binding
-				Port boundOutPort = nestedOutPorts.get(binding);
-
-				if (boundOutPort == null) {
-				    //throw new Exception("No @out corresponding to @in " + binding);
-					continue;
-				}
-				
-			    String outProgramName = programNameForPort.get(boundOutPort);
-				Program outProgram = programForName.get(outProgramName);
-				
-				// iterate over @in ports that bind to the current @out port
-				for (Port inPort : boundInPorts) {
-				
-					// get information about this @in port
-					String inProgramName = programNameForPort.get(inPort);
-					Program inProgram = programForName.get(inProgramName);
-	
-					// store the new channel
-					Channel channel = new Channel(outProgram, boundOutPort, inProgram, inPort);
-					nestedChannels.add(channel);
-				}	
-			}
+//			pruneUnusedWorkflowInPorts();
+//			pruneUnusedWorkflowOutPorts();
+			pruneUnusedNestedProgramInPorts();
+			pruneUnusedNestedProgramOutPorts();
+			buildNestedChannels();
 			
 			return new Workflow(
                 beginComment,
                 endComment,
-                inPorts,
-                outPorts,
+                workflowInPorts,
+                workflowOutPorts,
 				nestedPrograms,
 				nestedChannels
 			);
 		}
+	
+        private void pruneUnusedNestedProgramInPorts() {
+
+            List<String> unmatchedInBindings = new LinkedList<String>();
+            for (Map.Entry<String, List<Port>> entry : nestedProgramInPorts.entrySet()) {
+                String binding = entry.getKey();
+                if (!workflowInPorts.contains(binding) && !nestedProgramOutPorts.containsKey(binding)) {
+                    
+                    stderrStream.println(
+                            "WARNING: No nested @out port and no workflow @in port for nested @in '"    +
+                            binding                                                                     +
+                            "' on '"                                                                    +
+                            beginComment.programName                                                    +
+                            "'"
+                    );
+                    unmatchedInBindings.add(binding);
+                }
+            }
+            for (String binding : unmatchedInBindings) nestedProgramInPorts.remove(binding);
+        }
+	    
+        private void pruneUnusedNestedProgramOutPorts() {
+
+            List<String> unmatchedOutBindings = new LinkedList<String>();
+            
+            for (Entry<String, Port> entry : nestedProgramOutPorts.entrySet()) {
+                String binding = entry.getKey();
+                if (!workflowOutPorts.contains(binding) && !nestedProgramInPorts.containsKey(binding)) {
+                    
+                    stderrStream.println(
+                            "WARNING: No nested @in port and no workflow @out port for nested @out '"   +
+                            binding                                                                     +
+                            "' in workflow '"                                                           +
+                            beginComment.programName                                                    +
+                            "'"
+                    );
+                    unmatchedOutBindings.add(binding);
+                }
+            }
+            
+            for (String binding : unmatchedOutBindings) nestedProgramOutPorts.remove(binding);
+        }	
+        
+        private void buildNestedChannels() throws Exception {
+            
+            // build the channels between in and out ports
+            for (Map.Entry<String, List<Port>> entry : nestedProgramInPorts.entrySet()) {
+    
+                String binding = entry.getKey();
+                List<Port> boundInPorts = entry.getValue();
+    
+                // get information about the @out port that writes to this binding
+                Port boundOutPort = nestedProgramOutPorts.get(binding);
+                if (boundOutPort == null) throw new Exception("No @out corresponding to @in " + binding);
+                
+                String outProgramName = boundOutPort.beginComment.programName;
+                Program outProgram = programForName.get(outProgramName);
+                
+                // iterate over @in ports that bind to the current @out port
+                for (Port inPort : boundInPorts) {
+                
+                    // get information about this @in port
+                    String inProgramName = inPort.beginComment.programName;
+                    Program inProgram = programForName.get(inProgramName);
+    
+                    // store the new channel
+                    Channel channel = new Channel(outProgram, boundOutPort, inProgram, inPort);
+                    nestedChannels.add(channel);
+                }   
+            }
+        }
 	}
+	
 }
 
