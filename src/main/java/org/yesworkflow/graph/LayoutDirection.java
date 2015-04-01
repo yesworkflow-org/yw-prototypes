b577f6507af6c86726ac728a8ea95dc50418b31c
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-04-01 15:19:39 -0700

    Added RL and BT graph layout options.

--- src/main/java/org/yesworkflow/graph/LayoutDirection.java
+++ src/main/java/org/yesworkflow/graph/LayoutDirection.java
@@ -2,19 +2,30 @@ package org.yesworkflow.graph;
 
 public enum LayoutDirection {
     
-    LEFT_TO_RIGHT,
-    TOP_TO_BOTTOM;
+    LR,
+    RL,
+    TB,
+    BT;
     
     public static LayoutDirection toLayoutDirection(Object pv) throws Exception {
                 
         if (pv instanceof LayoutDirection) return (LayoutDirection)pv;
         
         if (pv instanceof String) {
+            
             String pvstring = (String)pv; 
-            if (pvstring.equalsIgnoreCase("lr")) return LayoutDirection.LEFT_TO_RIGHT;
-            if (pvstring.equalsIgnoreCase("horizontal")) return LayoutDirection.LEFT_TO_RIGHT;
-            if (pvstring.equalsIgnoreCase("tb")) return LayoutDirection.TOP_TO_BOTTOM;
-            if (pvstring.equalsIgnoreCase("vertical")) return LayoutDirection.TOP_TO_BOTTOM;
+            
+            if (pvstring.equalsIgnoreCase("lr") ||pvstring.equalsIgnoreCase("lefttoright"))
+                return LayoutDirection.LR;
+            
+            if (pvstring.equalsIgnoreCase("rl") ||pvstring.equalsIgnoreCase("righttoleft"))
+                return LayoutDirection.RL;
+
+            if (pvstring.equalsIgnoreCase("tb") || pvstring.equalsIgnoreCase("toptobottom"))
+                return LayoutDirection.TB;
+            
+            if (pvstring.equalsIgnoreCase("bt") || pvstring.equalsIgnoreCase("bottomtotop"))
+                return LayoutDirection.BT;
         }
         
         throw new Exception("Unrecognized LayoutDirection: " + pv);
