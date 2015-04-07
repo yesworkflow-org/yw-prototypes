6f6c04a367c8193844796f8e4e17848ca4ee710a
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-04-07 16:46:05 -0700

    YW comment-script using @uri keyword

---
+++ src/test/resources/org/yesworkflow/testPortUris/SimulateDataCollection.py
@@ -0,0 +1,54 @@
+# @begin SimulateDataCollection
+# @in screeningResults
+# @out correctedImage
+# @out collectionLog
+# @out rejectionLog
+
+
+    # @begin LoadScreeningResults
+    # @in screeningResults
+    # @out sampleName
+    # @out sampleQuality
+    # @end LoadScreeningResults
+
+    # @begin CalculateStrategy
+    # @param sampleName
+    # @param sampleQuality
+    # @out acceptedSample
+    # @out rejectedSample
+    # @out numImages
+    # @out energies
+    # @end CalculateStrategy
+
+    # @begin CollectDataSet
+    # @param acceptedSample
+    # @param numImages
+    # @param energies
+    # @out sampleId
+    # @out energy
+    # @out frameNumber
+    # @out rawImage @uri file:data/{name}/images/raw/{energy}/image_{frame}.raw
+    # @end CollectDataSet
+
+    # @begin TransformImage
+    # @param sampleId
+    # @param energy
+    # @param frameNumber
+    # @in rawImage
+    # @out correctedImage @uri file:data/{id}/image_{eV}_{frameNumber}.img
+    # @end TransformImage
+
+    # @begin LogAverageImageIntensity
+    # @in correctedImage
+    # @param sampleId
+    # @param frameNumber
+    # @out collectionLog @uri file:/collectedImages.txt
+    # @end LogAverageSpotIntensity
+
+    # @begin LogRejectedSamples
+    # @in rejectedSample
+    # @out rejectionLog @uri file:/rejectedSamples.txt
+    # @end LogRejectedSamples
+
+
+# @end SimulateDataCollection
