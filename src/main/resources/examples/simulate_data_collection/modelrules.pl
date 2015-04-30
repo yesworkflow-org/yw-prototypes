a818caf86df5676781d3dbf5a320b9ed6173ce40
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-04-29 20:05:16 -0700

    Added rules.

--- src/main/resources/examples/simulate_data_collection/modelrules.pl
+++ src/main/resources/examples/simulate_data_collection/modelrules.pl
@@ -8,6 +8,9 @@ program_line_range(ProgramName, SourcePath, StartLine, EndLine) :-
         annotation(EndAnnotation, SourceId, EndLine, _, _),
         extract_source(SourceId, SourcePath).
 
+has_port(ProgramId, PortId) :-
+    has_out_port(ProgramId, PortId); has_in_port(ProgramId, PortId).
+
 % RULE: uri_variable_value
 % Used to obtain values observed for a variable in the URI template
 % for the given port on the given program
@@ -15,7 +18,7 @@ uri_variable_value(VariableValue, ProgramName, PortName, UriVariableName) :-
     uri_variable_value(_, UriVariableId, VariableValue),
     uri_variable(UriVariableId, UriVariableName, PortId),
     port(PortId, _, PortName, _),
-    has_out_port(ProgramId, PortId),
+    has_port(ProgramId, PortId),
     program(ProgramId, ProgramName, _, _).
 
 % RULE: uri_variable_unique_values
@@ -23,4 +26,18 @@ uri_variable_value(VariableValue, ProgramName, PortName, UriVariableName) :-
 uri_variable_unique_values(UniqueValues, ProgramName, PortName, UriVariableName) :-
     findall(Value, uri_variable_value(Value, ProgramName, PortName, UriVariableName), ValueList),
     sort(ValueList, UniqueValues).
+
+% RULE: program_output_resource
+program_output_resource(ProgramName, ResourceId) :-
+    program(ProgramId, ProgramName, _, _),
+    has_out_port(ProgramId, PortId),
+    port_connects_to_channel(PortId, ChannelId),
+    resource_channel(ResourceId, ChannelId).
+    
+program_output_uri(ProgramName, ResourceUri) :-
+    program_output_resource(ProgramName,ResourceId),
+    resource(ResourceId, ResourceUri).
+    
+
+ 
 
