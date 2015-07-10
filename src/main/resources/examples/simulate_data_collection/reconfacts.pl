3656533f81349510105ba9d20b8c86af3429b7fa
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-07-10 15:47:13 -0700

    Recon now finds and stores facts for resources with constant URIs.

--- src/main/resources/examples/simulate_data_collection/reconfacts.pl
+++ src/main/resources/examples/simulate_data_collection/reconfacts.pl
@@ -1,4 +1,8 @@
 
 % FACT: resource(resource_id, resource_uri).
+resource(1, 'calibration.img').
+resource(2, 'run/run_log.txt').
+resource(3, 'run/collected_images.csv').
+resource(4, 'run/rejected_samples.txt').
 
 % FACT: uri_variable_value(resource_id, uri_variable_id, uri_variable_value).
