c71685aa436e334eba49d9ae4343d2b006f7c928
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-08-11 11:57:10 -0700

    YesWorkflowDB and tests now use static references to table and column identifiers.

--- src/main/java/org/yesworkflow/db/YesWorkflowDB.java
+++ src/main/java/org/yesworkflow/db/YesWorkflowDB.java
@@ -9,31 +9,33 @@ import java.nio.file.Path;
 
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
-import org.jooq.Table;
 import org.jooq.impl.DSL;
 import org.yesworkflow.util.FileIO;
+import org.yesworkflow.db.Table;
 
-import static org.yesworkflow.db.Tables.*;
+import static org.yesworkflow.db.Column.*;
 
 @SuppressWarnings("unchecked")
 public class YesWorkflowDB {
 
     private static String CREATE_TABLES_SCRIPT_H2  = "org/yesworkflow/db/h2/createtables.h2";
+    @SuppressWarnings("unused")
     private static String CREATE_TABLES_SCRIPT_SQLITE  = "org/yesworkflow/db/sqlite/createtables.sqlite";
     private static String createTablesScript = CREATE_TABLES_SCRIPT_H2;
 
     private static String IN_MEMORY_DB_URL_H2 = "jdbc:h2:mem:";
+    @SuppressWarnings("unused")
     private static String IN_MEMORY_DB_URL_SQLITE = "jdbc:sqlite::memory:";
     private static String inMemoryDbUrl = IN_MEMORY_DB_URL_H2;
     
     private final Connection connection;
     private final Statement statement;
-    private final DSLContext db;
+    public final DSLContext jooq;
     
     public YesWorkflowDB(Connection connection) throws SQLException {
         this.connection = connection;
         this.statement = connection.createStatement();
-        this.db = DSL.using(this.connection, SQLDialect.H2);
+        this.jooq = DSL.using(this.connection, SQLDialect.H2);
     }
     
     public static YesWorkflowDB createVolatileDB() throws Exception {
@@ -99,7 +101,7 @@ public class YesWorkflowDB {
 
     public void insertSourceFile(int id, String sourceFilePath) {
 
-        db.insertInto(SOURCE_FILE, ID, PATH)
+        jooq.insertInto(Table.SOURCE_FILE, ID, PATH)
           .values(id, sourceFilePath)
           .execute();
     }
@@ -107,31 +109,23 @@ public class YesWorkflowDB {
     public void insertAnnotation(int id, int sourceFileId, Integer qualifiedAnnotationId,
             int lineNumber, String tag, String keyword, String value, String description) {
         
-        db.insertInto(ANNOTATION,
-                      ID, SOURCE_FILE_ID, QUALIFIES, LINE_NUMBER,
-                      TAG, KEYWORD, VALUE, DESCRIPTION)
+        jooq.insertInto(Table.ANNOTATION,
+                        ID, SOURCE_FILE_ID, QUALIFIES, LINE_NUMBER,
+                        TAG, KEYWORD, VALUE, DESCRIPTION)
           .values(id, sourceFileId, qualifiedAnnotationId, lineNumber, tag, keyword, value, description)
           .execute();
     }
 
     
-    public int getRowCount(Table<?> T) throws SQLException {
+    public int getRowCount(org.jooq.Table<?> T) throws SQLException {
         
-        return (int)db.selectCount()
+        return (int)jooq.selectCount()
                       .from(T)
                       .fetchOne()
                       .getValue(0);
     }
 
-    public int getRowCount(String T) throws SQLException {
-        
-        return (int)db.selectCount()
-                      .from(T)
-                      .fetchOne()
-                      .getValue(0);
-    }
-    
-    public boolean hasTable(Table<?> T) throws SQLException {
+    public boolean hasTable(org.jooq.Table<?> T) throws SQLException {
         Exception caught = null;
         try {
             getRowCount(T);
--- src/test/java/org/yesworkflow/db/TestYesWorkflowDB.java
+++ src/test/java/org/yesworkflow/db/TestYesWorkflowDB.java
@@ -3,11 +3,14 @@ package org.yesworkflow.db;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.yesworkflow.db.Table;
 
-import static org.jooq.impl.DSL.table;
+import static org.yesworkflow.db.Column.*;
 
+import org.jooq.Result;
 import org.yesworkflow.YesWorkflowTestCase;
 
+@SuppressWarnings("unchecked")
 public class TestYesWorkflowDB extends YesWorkflowTestCase {
     
     private Path testDirectoryPath;
@@ -35,28 +38,76 @@ public class TestYesWorkflowDB extends YesWorkflowTestCase {
     public void testCreateDBTables() throws Exception {
 //        YesWorkflowDB ywdb = YesWorkflowDB.openFileDB(testDirectoryPath.resolve("schema.db"));
         YesWorkflowDB ywdb = YesWorkflowDB.createVolatileDB();
-        assertTrue(ywdb.hasTable(table("source_file")));
+        assertTrue(ywdb.hasTable(Table.SOURCE_FILE));
         ywdb.close();
     }
 
+    @SuppressWarnings("rawtypes")
     public void testInsertSourceFile() throws Exception {
         YesWorkflowDB ywdb = YesWorkflowDB.createVolatileDB();
-        assertEquals(0, ywdb.getRowCount(table("source_file")));
+        assertEquals(0, ywdb.getRowCount(Table.SOURCE_FILE));
         ywdb.insertSourceFile(1, "source1");
-        assertEquals(1, ywdb.getRowCount(table("source_file")));
+        assertEquals(1, ywdb.getRowCount(Table.SOURCE_FILE));
         ywdb.insertSourceFile(2, "source2");
-        assertEquals(2, ywdb.getRowCount(table("source_file")));
+        assertEquals(2, ywdb.getRowCount(Table.SOURCE_FILE));
         ywdb.insertSourceFile(3, "source3");
-        assertEquals(3, ywdb.getRowCount("source_file"));
+        assertEquals(3, ywdb.getRowCount(Table.SOURCE_FILE));
+        
+        Result r = ywdb.jooq.select(ID, PATH)
+                            .from(Table.SOURCE_FILE)
+                            .fetch();
+        
+        assertEquals(3, r.size());
+        assertEquals(1, (int) r.getValue(0, ID));
+        assertEquals("source1", (String) r.getValue(0, PATH));
+        assertEquals(2, (int) r.getValue(1, ID));
+        assertEquals("source2", (String) r.getValue(1, PATH));
+        assertEquals(3, (int) r.getValue(2, ID));
+        assertEquals("source3", (String) r.getValue(2, PATH));
     }
 
+    @SuppressWarnings("rawtypes")
     public void testInsertAnnotation() throws Exception {
-        YesWorkflowDB ywdb = YesWorkflowDB.createVolatileDB();
-        ywdb.insertSourceFile(1, "source");
-        ywdb.insertAnnotation(1, 1, 1, 10, "BEGIN", "begin", "p", null);
-        ywdb.insertAnnotation(2, 1, 1, 20, "END", "end", "p", null);
-        assertEquals(1, ywdb.getRowCount("source_file"));
-        assertEquals(2, ywdb.getRowCount("annotation"));
         
+        YesWorkflowDB ywdb = YesWorkflowDB.createVolatileDB();
+        
+        ywdb.insertSourceFile(1, "source1");
+        ywdb.insertSourceFile(2, "source2");
+        ywdb.insertAnnotation(1, 1, 1, 10, "BEGIN", "begin", "p", null);
+        ywdb.insertAnnotation(2, 1, 1, 20, "END",   "end",   "p", null);
+        ywdb.insertAnnotation(3, 2, 1, 5,  "BEGIN", "begin", "q", null);
+        ywdb.insertAnnotation(4, 2, 1, 15, "END",   "end",   "q", null);
+        
+        assertEquals(2, ywdb.getRowCount(Table.SOURCE_FILE));
+        assertEquals(4, ywdb.getRowCount(Table.ANNOTATION));
+
+        Result r = ywdb.jooq.select(ANNOTATION.ID, PATH, LINE_NUMBER, TAG, KEYWORD, VALUE)
+                            .from(Table.ANNOTATION)
+                            .join(Table.SOURCE_FILE)
+                            .on(ANNOTATION.SOURCE_FILE_ID.equal(SOURCE_FILE.ID))
+                            .fetch();
+
+        assertEquals(4, r.size());
+
+        assertEquals(1, r.getValue(0, ANNOTATION.ID));
+        assertEquals("source1", r.getValue(0, PATH));
+        assertEquals(10, r.getValue(0, LINE_NUMBER));
+        assertEquals("begin", r.getValue(0, KEYWORD));
+        
+        assertEquals(4, r.getValue(3, ANNOTATION.ID));
+        assertEquals("source2", r.getValue(3, PATH));
+        assertEquals(15, r.getValue(3, LINE_NUMBER));
+        assertEquals("end", r.getValue(3, KEYWORD));
+        
+        assertEquals(
+            "+-------------+-------+-----------+-----+-------+-----+"   + EOL +
+            "|annotation.id|path   |line_number|tag  |keyword|value|"   + EOL +
+            "+-------------+-------+-----------+-----+-------+-----+"   + EOL +
+            "|1            |source1|10         |BEGIN|begin  |p    |"   + EOL +
+            "|2            |source1|20         |END  |end    |p    |"   + EOL +
+            "|3            |source2|5          |BEGIN|begin  |q    |"   + EOL +
+            "|4            |source2|15         |END  |end    |q    |"   + EOL +
+            "+-------------+-------+-----------+-----+-------+-----+",
+            r.toString());
     }
 }
