5422c828e7a6473b5a183f6f65b865a935230f73
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-08-11 14:04:41 -0700

    Source class now inserts data into YWDB in factory method.

--- src/main/java/org/yesworkflow/extract/Source.java
+++ src/main/java/org/yesworkflow/extract/Source.java
@@ -1,9 +1,19 @@
 package org.yesworkflow.extract;
 
+import org.yesworkflow.db.YesWorkflowDB;
+
 public class Source {
+
+    private static Integer nextSourceId = 1;
 
     public final Integer id;
     public final String path;
+    
+    public static Source newSource(YesWorkflowDB ywdb, String path) {
+        Source source = new Source(nextSourceId++, "path");
+        ywdb.insertSourceFile(source.id, source.path);
+        return source;
+    }
     
     public Source(Integer id, String path) {
         this.id = id;
