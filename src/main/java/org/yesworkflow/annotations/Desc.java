fde0973876cff2dd7f209685e04995e29a3bb33b
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-12-17 18:47:25 -0800

    Desc now assigns description to the annotation it qualifies.

--- src/main/java/org/yesworkflow/annotations/Desc.java
+++ src/main/java/org/yesworkflow/annotations/Desc.java
@@ -1,16 +1,40 @@
 package org.yesworkflow.annotations;
 
+import java.util.StringTokenizer;
 import org.yesworkflow.YWKeywords;
-import org.yesworkflow.YWKeywords.Tag;
 
 public class Desc extends Qualification {
     
+    protected String description = null;
+    
     public Desc(Long id, Long sourceId, Long lineNumber,String comment, Annotation primaryAnnotation) throws Exception {
         super(id, sourceId, lineNumber, comment, YWKeywords.Tag.DESC, primaryAnnotation);
+        StringTokenizer commentTokens = new StringTokenizer(comment);
+        commentTokens.nextToken();
+        primaryAnnotation.description = buildDescription(commentTokens);
     }
 
-    protected Desc(Long id, Long sourceId, Long lineNumber,String comment, Tag tag, Annotation primaryAnnotation) throws Exception {
-        super(id, sourceId, lineNumber,comment, tag, primaryAnnotation);
+    public void appendDescription(String extraDescription) {
+        if (extraDescription != null) {
+            if (description == null) {
+                description = extraDescription;
+            } else {
+                description += " " + extraDescription;
+            }
+        }
+    }
+    
+    public String description() {
+        return description;
+    }
+    
+    private String buildDescription(StringTokenizer commentTokens) {
+        StringBuilder descriptionBuilder = new StringBuilder();
+        while (commentTokens.hasMoreTokens()) {
+            descriptionBuilder.append(' ').append(commentTokens.nextToken());
+        }
+        String description = descriptionBuilder.toString().trim();
+        return (description.length() > 0) ? description : null;
     }
     
     public String toString() {
---
+++ src/test/java/org/yesworkflow/annotations/TestDesc.java
@@ -0,0 +1,46 @@
+package org.yesworkflow.annotations;
+
+import org.yesworkflow.YesWorkflowTestCase;
+import org.yesworkflow.annotations.In;
+
+public class TestDesc extends YesWorkflowTestCase {
+
+    public void testBeginWithouDesc() throws Exception {
+        Begin begin = new Begin(1L, 1L, 1L, "@begin main");
+        assertEquals("main", begin.name);
+        assertNull(begin.description);
+    }    
+
+    public void testBeginComment_NameAndOneWordDescription() throws Exception {
+        Begin begin = new Begin(1L, 1L, 1L, "@begin main");
+        new Desc(2L, 1L, 1L, "@desc Distance calculator", begin);
+        assertEquals("main", begin.name);
+        assertEquals("Distance calculator", begin.description);
+    }
+    
+    public void testInWithoutDesc() throws Exception {
+        In in = new In(1L, 1L, 1L, "@in x ");
+        assertEquals("x", in.name);
+        assertNull(in.description());
+    }
+
+    public void testInWithDesc() throws Exception {
+        In in = new In(1L, 1L, 1L, "@in x ");
+        new Desc(2L, 1L, 1L, "@desc Horizontal distance", in);
+        assertEquals("x", in.name);
+        assertEquals("Horizontal distance", in.description().toString());
+    }
+
+    public void testOutWithoutDesc() throws Exception {
+        Out out = new Out(1L, 1L, 1L, "@out y ");
+        assertEquals("y", out.name);
+        assertNull(out.description());
+    }
+
+    public void testOutWithDesc() throws Exception {
+        Out out = new Out(1L, 1L, 1L, "@out y ");
+        new Desc(2L, 1L, 1L, "@desc Vertical distance", out);
+        assertEquals("y", out.name);
+        assertEquals("Vertical distance", out.description().toString());
+    }
+}
