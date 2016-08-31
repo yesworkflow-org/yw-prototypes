6705e5eb260e3a18ec4a0d0701485caf0b90bb5e
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-08-31 12:02:50 -0700

    Added factory for elaboratin a Program to yeild a Workflow.

--- src/main/java/org/yesworkflow/model/Workflow.java
+++ src/main/java/org/yesworkflow/model/Workflow.java
@@ -28,9 +28,54 @@ public class Workflow extends Program {
              functions.toArray(new Function[functions.size()]));
     }
 
+    public Workflow(Program p) {
+    	 super(	p.id, 
+    			p.name, 
+    			p.beginAnnotation, 
+    			p.endAnnotation, 
+                p.data,
+                p.inPorts,
+                p.outPorts,
+                p.programs,
+                p.channels,
+                p.functions
+                );
+    }
+
+    public Workflow(Long id, String name, Begin beginAnnotation, End endAnnotation, Data[] data, Port[] inPorts,
+			Port[] outPorts, Program[] programs, Channel[] channels, Function[] functions) {
+	   	 super(	id, name, beginAnnotation, endAnnotation, data,
+	         	inPorts, outPorts, programs, channels, functions);	
+	}
+
+	public static Workflow createFromProgram(Program p) {
+		Program[] programs;
+    	 if (p.programs.length > 0) {
+    		 programs = p.programs;
+    	 } else {
+    		 Program program = new Program(80L, p.name, p.beginAnnotation, p.endAnnotation, 
+    				 					   new Data[] {}, new Port[] {}, new Port[] {}, 
+    				 					   new Program[] {}, new Channel[] {}, new Function[] {});
+    		 programs = new Program[] { program };
+    	 }
+    	 
+    	 return new Workflow(
+    			 		p.id, 
+    	    			p.name, 
+    	    			p.beginAnnotation, 
+    	    			p.endAnnotation, 
+    	                p.data,
+    	                p.inPorts,
+    	                p.outPorts,
+    	                programs,
+    	                p.channels,
+    	                p.functions
+    	                );
+    	 
+    }
+    
     @Override
     public boolean isWorkflow() {
         return true;
     }
 }
-
